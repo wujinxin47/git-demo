20dbe48c711939274fdf6c877b930bfc3fa40c48
wujinxin47 <2597511366@qq.com>
2025-07-18 18:19:45 +0800

    add function

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 let demo = "this is a demo";
+
+// 新增：打印 demo 变量内容
+console.log(demo);
+
+// 新增：定义一个简单的加法函数
+function add(a, b) {
+    return a + b;
+}
+
+// 新增：调用加法函数并打印结果
+console.log("2 + 3 = " + add(2, 3));
